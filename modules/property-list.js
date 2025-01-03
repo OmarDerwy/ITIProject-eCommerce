b0e35513d0de6b578f89adf051121b7565a9fbc3
OmarDerwy <OmarDerwy@gmail.com>
2025-01-03 23:25:31 +0200

    create basic page which lists locations from properties.json

---
+++ modules/property-list.js
@@ -0,0 +1,18 @@
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+
+//loop through locations in properties.json and create p elements under .property-container
+
+
+fetch("./assets/properties.json")
+    .then((res) => res.json())
+    .then((data) => {
+        const propertyContainer = document.querySelector(".property-container");
+        data.forEach((property) => {
+            const pElement = document.createElement("p");
+            pElement.textContent = property.location;
+            propertyContainer.appendChild(pElement);
+        });
+    })
+    .catch((err) => {
+        console.error("Error fetching data:", err);
+    });
