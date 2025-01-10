dc5c9e05ee0aeb5409497b185b13b236b2320023
Tarek Hassanin <hassanin.trk@gmail.com>
2025-01-11 00:46:54 +0200

    SCRUM-29 CSS of Property Form

--- modules/propertyForm.js
+++ modules/propertyForm.js
@@ -1,9 +1,23 @@
 // Function to enable the selection of floor number for only flats 
-const type = document.getElementById("propertyType");
-const floor = document.getElementById("floor");        
+const adTitle = document.getElementById('adTitle');
+const propertyPrice = document.getElementById('propertyPrice');
+const depositePrice = document.getElementById('depositePrice');
+const city = document.getElementById('city');
+const state = document.getElementById('state');
+const propertyType = document.getElementById('propertyType');
+const floor = document.getElementById("floor");   
+const bathrooms = document.getElementById('bathrooms');
+const bedrooms = document.getElementById('bathrooms');
+const propertyStatus = document.getElementById('propertyStatus');
+const propertyArea = document.getElementById('propertyArea');
+const propertyDescription = document.getElementById('propertyDescription');
+
+
+
+// Function to enable the selection of floor number for only flats 
 floor.disabled = true;
 type.addEventListener('change', () => {
-    if(type.value === "flat")
+    if(propertyType.value === "flat")
         floor.disabled = false;
     else
         floor.disabled = true;
