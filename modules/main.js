bbc5ff7d5b9d0848d027726e24d794a0250c7c09
OmarDerwy <OmarDerwy@gmail.com>
2025-01-12 02:50:51 +0200

    modify navbar and footer as components

--- modules/main.js
+++ modules/main.js
@@ -4,26 +4,7 @@ import { featuredProperties } from "./featuredProperties.js";
 /* ------------------------------------------------- NAV BAR -------------------------------------------------------- */
 
 // dropdown menu 
-const toggleBtn = document.querySelector('.toggle-btn');
-const toggleBtnIcon = document.querySelector(".toggle-btn i");
-const dropDownMenu = document.querySelector(".dropdown-menu");
-const navbar = document.querySelector("header")
 
-toggleBtn.onclick = function () {
-    dropDownMenu.classList.toggle('open');
-
-    const isOpen = dropDownMenu.classList.contains('open');
-
-    if(isOpen){
-        toggleBtnIcon.classList = 'fa-solid fa-x';
-        navbar.style = 'margin-bottom:280px;'
-
-    }
-    else{
-        toggleBtnIcon.classList = 'fa-solid fa-bars';
-        navbar.style = 'margin-bottom:0px;'
-    }
-}
 
 /* ------------------------------------------------- HERO SECTION ---------------------------------------------------- */
 
---
+++ modules/onclick.js
@@ -0,0 +1,18 @@
+function handleToggleButton() {
+    const toggleBtnIcon = document.querySelector(".toggle-btn i");
+    const dropDownMenu = document.querySelector(".dropdown-menu");
+    const navbar = document.querySelector("header")
+    dropDownMenu.classList.toggle('open');
+
+    const isOpen = dropDownMenu.classList.contains('open');
+
+    if(isOpen){
+        toggleBtnIcon.classList = 'fa-solid fa-x';
+        navbar.style = 'margin-bottom:280px;'
+
+    }
+    else{
+        toggleBtnIcon.classList = 'fa-solid fa-bars';
+        navbar.style = 'margin-bottom:0px;'
+    }
+}
